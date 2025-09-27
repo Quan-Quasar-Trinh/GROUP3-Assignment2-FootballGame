e03e4ffcef96f22f9e4fd51e610de8e6f05bff42
Quan-Quasar-Trinh <billynhatquan@gmail.com>
2025-09-27 18:31:27 +0700

    change friction

--- Class/ball.py
+++ Class/ball.py
@@ -8,7 +8,7 @@ class Ball:
         
     def update(self):
         self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
-        self.velocity = (self.velocity[0] * 0.98, self.velocity[1] * 0.98)  # Simulate friction
+        self.velocity = (self.velocity[0] * 0.975, self.velocity[1] * 0.975)  # Simulate friction
         
     def draw(self, surface):
         pygame.draw.circle(surface, (255, 255, 0), self.position, 15)  # Draw ball as a yellow circle    
